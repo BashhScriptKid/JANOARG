5f2997b37c7772fb04a61992551ed5b35afe20eb
BashhScriptKid <dev@bashh.slmail.me>
2026-04-28 19:54:20 +0800

    Keep Result to use normal GraphicCircle for compatibility reasons

--- Assets/JANOARG.Client/Scripts/Behaviors/Player/PlayerScreenResult.cs
+++ Assets/JANOARG.Client/Scripts/Behaviors/Player/PlayerScreenResult.cs
@@ -33,7 +33,7 @@ namespace JANOARG.Client.Behaviors.Player
         public RectTransform       RankHolder;
         public TMP_Text            RankText;
         public List<Image>         ScoreRings;
-        public List<GraphicCircleGPU> ScoreExplosionRings;
+        public List<GraphicCircle> ScoreExplosionRings;
         public RectTransform       ScoreBarHolder;
         public RectTransform       ScoreBarFill;
         public List<RectTransform> ScoreBarMarks;
@@ -120,7 +120,7 @@ namespace JANOARG.Client.Behaviors.Player
             ResultTextBig.text = ResultText.text;
             ResultTextBig.alpha = 0;
 
-            foreach (GraphicCircleGPU ring in ScoreExplosionRings)
+            foreach (GraphicCircle ring in ScoreExplosionRings)
                 ring.rectTransform.localPosition = Vector2.zero;
 
             ScoreExplosionRings[0].color = ScoreExplosionRings[1].color =
