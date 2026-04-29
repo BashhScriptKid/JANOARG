49b884e353dd2ad56ca5b5bc81dc219b5a8df8ae
BashhScriptKid <dev@bashh.slmail.me>
2026-04-29 15:35:25 +0800

    Fix scoring algorithm being unintentionally rounded for no reason

--- Assets/JANOARG.Client/Scripts/Behaviors/Player/PlayerScreen.cs
+++ Assets/JANOARG.Client/Scripts/Behaviors/Player/PlayerScreen.cs
@@ -1101,7 +1101,7 @@ namespace JANOARG.Client.Behaviors.Player
             
             double offsetAbs = Math.Abs(offset);
             float? accuracy = null;
-            int finalScore;
+            float finalScore;
 
             // Handle different hit evaluation types
             if (isFlickable || isCatchType)
@@ -1116,11 +1116,11 @@ namespace JANOARG.Client.Behaviors.Player
             }
             else
             {
-                // Gradual accuracy evaluation
                 accuracy = CalculateAccuracy(offset, offsetAbs);
-                finalScore = Mathf.RoundToInt(baseScore * (1 - Mathf.Abs(accuracy.Value)));
+                finalScore = baseScore * (1 - Mathf.Abs(accuracy.Value));
                 
                 AddScore(finalScore, accuracy);
+                
                 HitObjectHistory.Add(new HitObjectHistoryItem(hitObject, offset));
             }
 
